df0571d26b4fe65244e13369df955d856a78d334
ssy <sm-dg-ku@hanmail.com>
2022-10-24 14:47:45 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,15 +7,15 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-#CURRENT_PID=$(pgrep -fl book | grep jar | awk '{print $1}')
-CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+CURRENT_PID=$(pgrep -fl book | grep jar | awk '{print $1}')
+#CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> Kill -15 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
@@ -29,14 +29,14 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-#nohup java -jar \
-#  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-#  -Dspring.profiles.active=real \
-#  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profile.active=real \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+#nohup java -jar \
+#  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+#  -Dspring.profile.active=real \
+#  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 # [$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &]
 # -nohup 실행 시 CodeDeploy는 무한 대기
