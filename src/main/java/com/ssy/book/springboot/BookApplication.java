3d1bbdce9d948d84ada6ced9ccdf134e126acfb0
ssy <sm-dg-ku@hanmail.com>
2022-10-24 16:19:40 +0900

    embedded.tomcat.TomcatStarter 에러 건';

--- src/main/java/com/ssy/book/springboot/BookApplication.java
+++ src/main/java/com/ssy/book/springboot/BookApplication.java
@@ -2,13 +2,18 @@ package com.ssy.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.system.ApplicationPid;
 
 //@EnableJpaAuditing // JPA Auditing 활성화 -> 별도 Config로 분리
 @SpringBootApplication
 public class BookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
+		// SpringApplication.run(BookApplication.class, args);
+		SpringApplication application = new SpringApplication(BookApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
