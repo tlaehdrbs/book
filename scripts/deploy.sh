e53b2d12437d3e03175214eddccddf9b5ee52b93
ssy <sm-dg-ku@hanmail.com>
2022-10-24 14:35:11 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,8 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl book | grep jar | awk '{print $1}')
+#CURRENT_PID=$(pgrep -fl book | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -20,18 +21,21 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
+  echo "> $JAR_NAME 에 실행권한 추가"
+  chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
+#nohup java -jar \
+#  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+#  -Dspring.profiles.active=real \
+#  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=real \
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profile.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 # [$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &]
