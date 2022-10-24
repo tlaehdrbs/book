1bb681d330564c44b237eede2fb885ec2e183e6e
ssy <sm-dg-ku@hanmail.com>
2022-10-24 14:40:10 +0900

    들여쓰기 에러 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,8 +25,8 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-  echo "> $JAR_NAME 에 실행권한 추가"
-  chmod +x $JAR_NAME
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 #nohup java -jar \
